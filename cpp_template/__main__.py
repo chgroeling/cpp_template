50dbbfc44c8ee633a0bf5b1cf0f33df14aebb723
Christian Gröling <contact@christiangroeling.de>
2023-04-07 10:00:01 +0200

    Simple hello world app can be generated

--- cpp_template/__main__.py
+++ cpp_template/__main__.py
@@ -1,17 +1,40 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
+from pathlib import Path
+
+project_def = {
+    "PROJECT_NAME": "cpp_template",
+    "PROJECT_VERSION": "0.3",
+    "PROJECT_DESCRIPTION": "Test Project",
+    "EXTERN": {
+        "doxygen": False,
+        "googletest" : False,
+        "cxx_opts": False,
+        "fmt": False,
+        "spdlog": False,
+        "PCRE2": False,
+    },
+}
+
 env = Environment(
-    loader=PackageLoader("cpp_proj"),
-    autoescape=select_autoescape()
+    loader=PackageLoader("cpp_proj"), trim_blocks=True, lstrip_blocks=True
 )
 
-print("HELLO world")
 
-template = env.get_template("CMakeLists.txt")
+def create(template_name, filepos):
+    template_ = env.get_template(template_name)
+    out_template = template_.render(project_def)
 
-out_template = template.render(PROJECT_NAME="variables", go="here")
+    # to save the results
+    with open(filepos, "w") as fh:
+        fh.write(out_template)
 
-print(out_template)
 
-# to save the results
-with open("tmp/CMakeLists.txt", "w") as fh:
-    fh.write(out_template)
+# Create directory structure
+Path("./tmp/app/").mkdir(parents=True, exist_ok=True)
+Path("./tmp/src/").mkdir(parents=True, exist_ok=True)
+Path("./tmp/include/").mkdir(parents=True, exist_ok=True)
+
+
+create("CMakeLists.txt", "tmp/CMakeLists.txt")
+create("app/CMakeLists.txt", "tmp/app/CMakeLists.txt")
+create("app/main.cpp", "tmp/app/main.cpp")
